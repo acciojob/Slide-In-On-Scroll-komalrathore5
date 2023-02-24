d499c2c611f8c8cd279e0fab0307b3de498414f9
komalrathore5 <51449967+komalrathore5@users.noreply.github.com>
2023-02-24 22:38:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Your JS code here.
+function addSlide() {
+            var scrolledImages = document.querySelectorAll('.slide-in');
+            console.log("ScrolledImages",scrolledImages);
+            scrolledImages.forEach((img) => {
+                var scrolledHeight = window.scrollY + window.innerHeight - img.height / 2;
+                var imageBottomHeight = img.offsetTop + img.height;
+                var isImageHalfScrolled = scrolledHeight > img.offsetTop;
+
+                var isImageFullyScrolled = window.scrollY > imageBottomHeight;
+               
+                console.log("Height", scrolledHeight,imageBottomHeight, img.offsetTop, window.innerHeight);
+                if (isImageHalfScrolled && !isImageFullyScrolled) {
+                    img.classList.add("active");
+                } else {
+                    img.classList.remove("active");
+                }
+            });
+        }
+        window.addEventListener('scroll', debounce(addSlide));
